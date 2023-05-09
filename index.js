04b1a99f66753aa367857f6126b3d0ebf06cbd9b
webpawan <pawansaini66477@gmail.com>
2023-05-09 21:12:58 +0530

    user registration backend

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+const PORT = process.env.PORT || 3000;
+import "./DataBase/connection.js";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(express.json());
+import userRouter from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
+
+app.use(cookieParser())
+app.use("/api/user", userRouter);
+
+app.listen(PORT, () => {
+  console.log(PORT + " " + "server start");
+});
